d6c2e8e5fb18511700e306909441c302c10d352c
latimerk <latimer1@uw.edu>
2015-11-16 16:55:11 -0800

    attempting to finalize stimulus block 1

--- code/StimulusGeneration/PoissonPulses/makePoissonPulseStimulusBlocks.m
+++ code/StimulusGeneration/PoissonPulses/makePoissonPulseStimulusBlocks.m
@@ -10,7 +10,7 @@ frozenNoiseInfo.minRate = 0.5;
 frozenNoiseInfo.maxRate = 20;
 
 generationInfo.randomSeed        = 81473;
-generationInfo.randomSeed_frozen = 12699;
+generationInfo.randomSeed_frozen = 21796;
 generationInfo.upsampleRate      = 1;
 [Stim,trialLength,trialFrequency,~,frozenNoiseTrials,frozenNoiseRate] = generatePoissonStimSet(nTrials,frozenNoiseInfo,1,generationInfo.randomSeed,generationInfo.randomSeed_frozen,[],[],frameLen);
 generationInfo.stimGenerateCommand = sprintf('[Stim,trialLength,TF,~,frozenNoiseTrials] = generatePoissonStimSet(%d,frozenNoiseInfo,1,%d,%d,[],[],frameLen)',nTrials,generationInfo.randomSeed,generationInfo.randomSeed_frozen);
@@ -20,5 +20,6 @@ generationInfo.createdOn   = datestr(now);
 
 generationInfo.upsampleRate = 2;
 [Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,generationInfo.upsampleRate);
-generationInfo.stimGenerateCommand = sprintf('%s\n[Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,generationInfo.upsampleRate);',generationInfo.stimGenerateCommand);
+frozenNoiseRate             = upsampleTrialBlock(frozenNoiseRate',[],[],generationInfo.upsampleRate)';
+generationInfo.stimGenerateCommand = sprintf('%s\n[Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,generationInfo.upsampleRate);\nfrozenNoiseRate             = upsampleTrialBlock(frozenNoiseRate'',[],[],generationInfo.upsampleRate)'';',generationInfo.stimGenerateCommand);
 %save(sprintf('Stimuli/PoissonPulse/StimBlock%d_upsampled.mat',stimBlock),'Stim','trialLength','trialFrequency','frozenNoiseTrials','frozenNoiseRate','frozenNoiseInfo','frameLen','generationInfo');
