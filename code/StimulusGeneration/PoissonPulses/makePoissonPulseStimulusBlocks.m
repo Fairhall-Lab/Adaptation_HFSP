59a4d82fbce48fd545748feff5e4a64d0336c7a1
latimerk <latimer1@uw.edu>
2015-11-16 16:48:58 -0800

    making sure stimulus files include all stimulus generation info

--- code/StimulusGeneration/PoissonPulses/makePoissonPulseStimulusBlocks.m
+++ code/StimulusGeneration/PoissonPulses/makePoissonPulseStimulusBlocks.m
@@ -11,12 +11,14 @@ frozenNoiseInfo.maxRate = 20;
 
 generationInfo.randomSeed        = 81473;
 generationInfo.randomSeed_frozen = 12699;
+generationInfo.upsampleRate      = 1;
 [Stim,trialLength,trialFrequency,~,frozenNoiseTrials,frozenNoiseRate] = generatePoissonStimSet(nTrials,frozenNoiseInfo,1,generationInfo.randomSeed,generationInfo.randomSeed_frozen,[],[],frameLen);
 generationInfo.stimGenerateCommand = sprintf('[Stim,trialLength,TF,~,frozenNoiseTrials] = generatePoissonStimSet(%d,frozenNoiseInfo,1,%d,%d,[],[],frameLen)',nTrials,generationInfo.randomSeed,generationInfo.randomSeed_frozen);
 generationInfo.gitRevision = getGitCommit();
 generationInfo.createdOn   = datestr(now);
 save(sprintf('Stimuli/PoissonPulse/StimBlock%d.mat',stimBlock),'Stim','trialLength','trialFrequency','frozenNoiseTrials','frozenNoiseRate','frozenNoiseInfo','frameLen','generationInfo');
 
-[Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,upsampleRate);
-generationInfo.stimGenerateCommand = sprintf('%s\n[Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,upsampleRate);',generationInfo.stimGenerateCommand);
+generationInfo.upsampleRate = 1;
+[Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,generationInfo.upsampleRate);
+generationInfo.stimGenerateCommand = sprintf('%s\n[Stim,trialLength,frameLen] = upsampleTrialBlock(Stim,trialLength,frameLen,generationInfo.upsampleRate);',generationInfo.stimGenerateCommand);
 save(sprintf('Stimuli/PoissonPulse/StimBlock%d_upsampled.mat',stimBlock),'Stim','trialLength','trialFrequency','frozenNoiseTrials','frozenNoiseRate','frozenNoiseInfo','frameLen','generationInfo');
